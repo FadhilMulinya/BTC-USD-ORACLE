ee743d6d889042073ab1210cda6632a5ce4a97c4
fadhil <134047249+FadhilMulinya@users.noreply.github.com>
2024-11-22 20:00:52 +0300

    Push of the final code

--- src/TokenICO.sol
+++ src/TokenICO.sol
@@ -15,5 +15,36 @@ contract TokenICO {
 
     constructor(address tokenAddress) {
         priceFeed = AggregatorV3Interface(tokenAddress);
+        minter = TokenInterface(tokenAddress);
+
+        owner = msg.sender;
     }
+
+    //Returns the amount of the Latest Eth/Usd Price.
+    function getChainLinkLatestData() public view returns(int256){
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        return answer;
+    }
+
+    //Returns the amount the user will get as accorrding to the ETh He/She Brought.
+    function tokenAmount(uint256 ethAmount) public view returns(uint256){
+        uint256 ethUsd = uint256(getChainLinkLatestData()); //will return in 8 decimals i.e 3930,00000000
+        uint256 amountUsd = (ethAmount * ethUsd) / 10**18;
+        uint256 amountToken = (amountUsd/tokenPrice)/ 10 **(8/2);
+        return amountToken;
+    }
+
+    receive() external payable {
+        uint256 amountToken = tokenAmount(msg.value);
+        minter.mint(msg.sender, amountToken);
+    }
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    function withdraw() public onlyOwner {
+        payable(owner).transfer(address(this).balance);
+    }
+
+
 }
